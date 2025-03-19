e2f9d7f2efb28741179cd3de33b60974eaf12565
paydownant <takase0211@gmail.com>
2025-03-19 17:49:41 +0900

    added smaller font sizes

--- src/gui.hpp
+++ src/gui.hpp
@@ -86,7 +86,7 @@ private:
 
     struct FontPixels {
         std::vector<ImFont*> fonts;
-        std::vector<int> sizes = { 6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40 };
+        std::vector<int> sizes = { 2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40 };
     };
 
     struct UIStyle {
